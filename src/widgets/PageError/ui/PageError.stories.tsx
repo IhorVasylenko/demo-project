6779170951087145b6962e1da999b6b2ca88be4d
IhorVasylenko <theone116355@gmail.com>
2023-02-20 21:24:10 +0200

    add storybook and decorators

---
+++ src/widgets/PageError/ui/PageError.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { PageError } from './PageError';
+
+export default {
+  title: 'widgets/PageError',
+  component: PageError,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof PageError>;
+
+const Template: ComponentStory<typeof PageError> = (args) => (
+  <PageError {...args} />
+);
+
+export const Light = Template.bind({});
+Light.args = {
+  error: {
+    message: 'Some Error',
+  },
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+  error: {
+    message: 'Some Error',
+  },
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
