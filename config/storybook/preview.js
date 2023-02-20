e5189449492a2e8ebf4780df641c3b7d0ecda806
IhorVasylenko <theone116355@gmail.com>
2023-02-20 23:19:57 +0200

    add storybook theme addon

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,6 +3,7 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from '../../src/app/providers/ThemeProvider';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+// import { I18nDecorator } from '../../src/shared/config/storybook/i18nDecorator/i18nDecorator';
 
 addDecorator(StyleDecorator);
 
@@ -14,8 +15,16 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  themes: {
+    default: Theme.LIGHT,
+    list: [
+      { name: Theme.LIGHT, class: ['app', 'light'], color: 'rgb(236 225 225 / 100%)' },
+      { name: Theme.DARK, class: ['app', 'dark'], color: 'rgb(1 1 90 / 100%)' },
+    ],
+  },
 };
 
 addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
+// addDecorator(I18nDecorator);
